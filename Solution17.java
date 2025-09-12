82ed2769c09be6e51a863cc43d0b29810fd8899a
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-09-12 15:37:43 +0600

    Create Solution17.java

---
+++ Solution17.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev=new StringBuilder(A).reverse().toString();
+        System.out.println(A.equals(rev)?"Yes":"No");
+    }
+}
